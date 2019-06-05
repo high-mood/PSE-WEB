799beb437e53cff91add44cb7e00c5e9cd86afa3
Diallo <mnl.diallo@gmail.com>
2019-06-06 00:26:58 +0200

    Added the user's information to a session along with account creation.

--- app/views.py
+++ app/views.py
@@ -1,13 +1,42 @@
-from flask import send_from_directory, jsonify, json, render_template, redirect, request, session,flash
+from flask import send_from_directory, jsonify, json, render_template, redirect, request, session, flash, url_for
 from app import app
 from app import db
 # Refactor later
 from app import models
-
-
-
+from app import spotifysso
+from app.API import spotify
 
 @app.route("/index", methods=['GET', 'POST'])
 @app.route("/", methods=['GET', 'POST'])
 def index():
     return "Hello World"
+
+
+@app.route("/login")
+def login():
+    return spotifysso.authorize(callback="http://localhost:5000/callback")
+    # return spotifysso.authorize(callback=url_for('authorized', _external=True, _scheme="https"))
+
+
+@app.route('/callback')
+def authorized():
+    resp = spotifysso.authorized_response()
+
+    # TODO: EXception handling (if exception or if None#)
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    if isinstance(resp, Exception):
+        return 'Access denied: error=%s' % str(resp)
+
+    access_token = resp['access_token']
+    refresh_token = resp['refresh_token']
+    scopes = resp['scope'].split(" ")
+
+    json_user_info = spotify.get_user_info(access_token)
+    models.User.create_if_not_exist(json_user_info)
+    session['json_info'] = json_user_info  # TODO change this laziness
+
+    return redirect(url_for('index'))
