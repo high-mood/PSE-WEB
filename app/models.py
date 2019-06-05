5351dd54bfd6f56052f6b0b4aa4dde23c41d7927
Diallo <mnl.diallo@gmail.com>
2019-06-06 00:41:58 +0200

    Added functionality for registering new users.

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 # Imports here
 
 from app import db
-
+import datetime
 """smaple response:
 
 {
@@ -35,3 +35,20 @@ class User(db.Model):
     is_premium = db.Column(db.Boolean(),default=False)
     refresh_token = db.Column(db.String(300))
     user_is_active = db.Column(db.Boolean())
+
+    @staticmethod
+    def create_if_not_exist(json_info, refresh_token):
+        user = User.query.filter_by(user_id=json_info['id']).first()
+        if user is None:
+            user = User(user_id=json_info['id'],
+                        email=json_info['email'],
+                        display_name=json_info['display_name'],
+                        image_url=None,
+                        birthdate=datetime.datetime.strptime(json_info['birthdate'], "%Y-%m-%d"),
+                        country=json_info['country'],
+                        is_premium=(json_info['product'] is "premium"),
+                        refresh_token=refresh_token,
+                        user_is_active=True)
+
+            db.session.add(user)
+            db.session.commit()
