83b7bee7a9b61f9ca653835223ecda8a2bf7a3c8
Diallo <mnl.diallo@gmail.com>
2019-06-05 22:06:42 +0200

    Changed app secret to be in config file

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import config
 
 app = Flask(__name__)
 app.config.from_object('config')
-app.secret_key = os.environ.get("APP_SECRET", "HELLO") # THIS SHOULD BE SOMETHING RANDOM
+app.secret_key = os.environ.get("APP_SECRET", config.SECRET) # THIS SHOULD BE SOMETHING RANDOM
 db = SQLAlchemy(app)
 from app import views
 
