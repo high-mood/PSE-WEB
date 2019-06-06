afcd089fbce897ad0a794e73c408b78761ef8d20
Diallo <mnl.diallo@gmail.com>
2019-06-06 13:33:50 +0200

    Added functionality for refreshing access token

--- app/API/spotify.py
+++ app/API/spotify.py
@@ -1,6 +1,8 @@
 import requests
-import ast
 
+from requests.auth import HTTPBasicAuth
+
+from config import SPOTIFY_CLIENT,SPOTIFY_SECRET
 
 def get_user_info(access_token):
     url = "https://api.spotify.com/v1/me"
@@ -14,3 +16,24 @@ def get_user_info(access_token):
                                     )
 
     return refresh_response.json()
+
+
+def get_access_token(refresh_token):
+    url = "https://accounts.spotify.com/api/token"
+
+
+
+    body = {"grant_type":"refresh_token",
+            "refresh_token":refresh_token}
+
+    response =  requests.post(url,data=body,auth=HTTPBasicAuth(SPOTIFY_CLIENT, SPOTIFY_SECRET))
+    return response.content
+
+"""
+{"access_token":"....",
+"token_type":"Bearer",
+"expires_in":3600,
+"scope":"user-library-read user-library-modify ...."}
+"""
+
+
