d35dd8c4bc67a9e31cb9c2dfe2e0dbfd38819398
Diallo <mnl.diallo@gmail.com>
2019-06-07 01:19:00 +0200

    Added token to function call

--- app/views.py
+++ app/views.py
@@ -20,7 +20,7 @@ def index():
         access_token = spotify.get_access_token(session['json_info']['refresh_token'])
 
         recently_played = spotify.get_recently_played(access_token)
-        top_songs = query.get_top_songs(client, userid, 10)
+        top_songs = query.get_top_songs(client, userid, 10,access_token)
         top_genres = query.get_top_genres(client, userid, 10)
         all_genres = query.get_genres(client, userid)
         total_listening_time = query.total_time_spent(client, userid)
