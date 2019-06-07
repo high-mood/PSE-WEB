8479f686c92f802ec60fea322665340571b4a115
Mounir <mounir9912@gmail.com>
2019-06-07 02:11:46 +0200

    trying to add sample pie chart for testing

--- app/templates/visualisation.js
+++ app/templates/visualisation.js
@@ -1,17 +1,17 @@
 new Chart(document.getElementById("myChart"), {
-	type: 'pie',
-	data: {
-		labels: ["hi1", "hi2", "hi3"],
-		datasets: [{
-			label: "testlabel (seconds)",
-			backgroundColor["red", "blue", "green"],
-			data: [1, 2, 3]
-		}]
-	},
-	options: {
-		title: {
-			display: true,
-			text: 'sample text'
-		}
-	}
+    type: 'pie',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [{
+        label: "Population (millions)",
+        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+        data: [2478,5267,734,784,433]
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
 });
