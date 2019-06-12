c9871ddb1d29b3d603f96ad99f8b45ec8e0b9adc
Diallo <mnl.diallo@gmail.com>
2019-06-13 00:36:00 +0200

    Changed API to utilize models instead

--- app/API/__init__.py
+++ app/API/__init__.py
@@ -1,9 +1,11 @@
+# TODO: restructur this code
+# TODO: unit tests
 import datetime
 import json
 
 from . import views
 from app import app
-from flask_restplus import Api, Resource
+from flask_restplus import Api, Resource, fields
 from flask import Blueprint, jsonify
 from app import models
 
@@ -11,24 +13,36 @@ blueprint = Blueprint('api', __name__, url_prefix='/api')
 api = Api(blueprint)
 app.register_blueprint(blueprint)
 
-name_space = api.namespace('Users', description='Main APIs')
+name_space = api.namespace('User', description='User information')
+
+model = api.model('UserInfo', {
+    'userid': fields.String,
+    'email': fields.String,
+    'display_name': fields.String,
+    'image_url': fields.String,
+    'birthdate': fields.DateTime,
+    'country': fields.String,
+    'is_premium': fields.Boolean,
+    'refresh_tokens': fields.String,
+    'user_is_active': fields.Boolean
+})
 
 
-@name_space.route("/<string:id>")
-class Users(Resource):
+@name_space.route("/<string:userid>")
+class MoodData(Resource):
 
-    @api.doc(responses={200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error'},
-             params={'id': 'Specify the Id associated with the person'})
-    def get(self,id):
-        us = models.User.query.filter_by(userid=id).first().__dict__
-        us.pop('_sa_instance_state', None)
+    @api.marshal_with(model, envelope='resource')
+    def get(self, userid):
+        """
+        Obtain all of a user's account information.
+        """
 
-
-        return json.dumps(us,cls=DateTimeEncoder)
-    def post(self):
-        return {
-            "status": "Posted new data"
-        }
+        user = models.User.query.filter_by(userid=userid).first()
+        return user
+# def post(self):
+#     return {
+#         "status": "Posted new data"
+#     }
 
 
 class DateTimeEncoder(json.JSONEncoder):
