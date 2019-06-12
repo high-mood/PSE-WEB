3e83e50e2b2d9040e767965366355ea263df1f36
Diallo <mnl.diallo@gmail.com>
2019-06-12 15:17:16 +0200

    Added working API for first user

--- app/API/__init__.py
+++ app/API/__init__.py
@@ -1,1 +1,39 @@
+import datetime
+import json
+
 from . import views
+from app import app
+from flask_restplus import Api, Resource
+from flask import Blueprint, jsonify
+from app import models
+
+blueprint = Blueprint('api', __name__, url_prefix='/api')
+api = Api(blueprint)
+app.register_blueprint(blueprint)
+
+name_space = api.namespace('Users', description='Main APIs')
+
+
+@name_space.route("/<string:id>")
+class Users(Resource):
+
+    @api.doc(responses={200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error'},
+             params={'id': 'Specify the Id associated with the person'})
+    def get(self,id):
+        us = models.User.query.filter_by(userid=id).first().__dict__
+        us.pop('_sa_instance_state', None)
+
+
+        return json.dumps(us,cls=DateTimeEncoder)
+    def post(self):
+        return {
+            "status": "Posted new data"
+        }
+
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, z):
+        if isinstance(z, datetime.datetime):
+            return (str(z))
+        else:
+            return super().default(z)
--- app/models.py
+++ app/models.py
@@ -27,7 +27,7 @@ import datetime
 # if not, boolean is active
 class User(db.Model):
     __tablename__ = "users"
-    user_id = db.Column(db.String(200), primary_key=True)
+    userid = db.Column(db.String(200), primary_key=True)
     email = db.Column(db.String(200))
     display_name = db.Column(db.String(200))
     image_url = db.Column(db.String(200))
@@ -39,9 +39,9 @@ class User(db.Model):
 
     @staticmethod
     def create_if_not_exist(json_info, refresh_token):
-        user = User.query.filter_by(user_id=json_info['id']).first()
+        user = User.query.filter_by(userid=json_info['id']).first()
         if user is None:
-            user = User(user_id=json_info['id'],
+            user = User(userid=json_info['id'],
                         email=json_info['email'],
                         display_name=json_info['display_name'],
                         image_url=None,
@@ -58,3 +58,82 @@ class User(db.Model):
     def get_all_tokes():
         query = db.session.query("refresh_token FROM users")
         return [row[0] for row in query]
+
+
+class Song(db.Model):
+    __tablename__ = "songs"
+    songid = db.Column(db.String(200), primary_key=True)
+    name = db.Column(db.String(300))
+
+    @staticmethod
+    def create_if_not_exist(json_info):
+        song = Song.query.filter_by(songid=json_info['songid']).first()
+        if song is None:
+            song = Song(songid=json_info['songid'],
+                        name=json_info['name'])
+
+            db.session.add(song)
+            db.session.commit()
+
+
+class Artist(db.Model):
+    __tablename__ = "artists"
+    artistid = db.Column(db.String(200), primary_key=True)
+    name = db.Column(db.String(300))
+    genres = db.Column(db.String(300))
+    popularity = db.Column(db.Integer())
+
+    @staticmethod
+    def create_if_not_exist(json_info):
+        artist = Artist.query.filter_by(artistid=json_info['artistid']).first()
+        if artist is None:
+            artist = Artist(artistid=json_info['artistid'],
+                            name=json_info['name'],
+                            genres=json_info['genres'],
+                            popularity=json_info['popularity'])
+
+            db.session.add(artist)
+            db.session.commit()
+
+
+class Songmood(db.Model):
+    __tablename__ = "songmoods"
+    songid = db.Column(db.String(200), primary_key=True)
+    excitedness = db.Column(db.Float())
+    happiness = db.Column(db.Float())
+
+    @staticmethod
+    def create_if_not_exist(json_info):
+        songmood = Songmood.query.filter_by(songid=json_info['songid']).first()
+        if songmood is None:
+            songmood = Songmood(songid=json_info['songid'],
+                                excitedness=json_info['excitedness'],
+                                happiness=json_info['happiness'])
+
+            db.session.add(songmood)
+            db.session.commit()
+
+    def get_moods(songids):
+        querystring = '(' + ','.join([f"'{id}'" for id in songids]) + ');'
+        excitedness = db.session.query('excitedness FROM songmoods where songid in ' + querystring)
+        happiness = db.session.query('happiness FROM songmoods where songid in ' + querystring)
+        return list(zip(excitedness, happiness))
+
+
+class SongArtist(db.Model):
+    __tablename__ = "songs_artists"
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    songid = db.Column(db.String(200), db.ForeignKey("songs.songid"))
+    artistid = db.Column(db.String(200), db.ForeignKey("artists.artistid"))
+
+    __table_args__ = (db.UniqueConstraint('songid', 'artistid', name='key'),)
+
+    @staticmethod
+    def create_if_not_exist(json_info):
+        songartist = SongArtist.query(f"select id from songs_artists where songid={songid} and artistid={artistid}").first()
+        if songartist is None:
+            songartist = SongArtist(songid=json_info['songid'],
+                                  artistid=json_info['artistid'])
+
+            db.session.add(songartist)
+            db.session.commit()
