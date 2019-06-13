1d6ad57d53dad68ae1a0f2429781d0225de19116
Jonasvdham <jonasvdham@gmail.com>
2019-06-13 19:01:17 +0200

    bug fixed

--- moodanalysis/serverMoodAnalysis.py
+++ moodanalysis/serverMoodAnalysis.py
@@ -1,16 +1,16 @@
 from sklearn.ensemble import GradientBoostingClassifier as GBC
 from joblib import load
 import numpy as np
+import csv
 
 ''' Mood classification for server, requires the .joblib files imported hereunder.'''
 
-E_est = load('moodanalysis/Trained-Excitedness.joblib')
-H_est = load('moodanalysis/Trained-Happiness.joblib')
-features = ["mode", "time_signature", "acousticness",
-        "danceability", "energy", "instrumentalness", "liveness", "loudness",
-        "speechiness", "valence", "tempo"]
-
 def analyse_mood(songs):
+    E_est = load('Trained-Excitedness.joblib')
+    H_est = load('Trained-Happiness.joblib')
+    features = ["mode", "time_signature", "acousticness",
+                "danceability", "energy", "instrumentalness", "liveness", "loudness",
+                "speechiness", "valence", "tempo"]
     input = []
     songtitles = []
     output = []
@@ -24,17 +24,13 @@ def analyse_mood(songs):
         inputdata = []
         for feature in features:
             inputdata.append(song['fields'][feature])
-        input.append(inputdata)
-    for val in input:
-        print(val)
+        input.append(np.array([(100*x) for x in inputdata]))
     ExcitednessPredictions = E_est.predict(input)
     HappinessPredictions = H_est.predict(input)
     for i in range(len(songtitles)):
-        print(ExcitednessPredictions)
         outputdata = {}
         outputdata['songid'] = songtitles[i]
         outputdata['excitedness'] = float(ExcitednessPredictions[i]) / 100
         outputdata['happiness'] = float(HappinessPredictions[i]) / 100
         output.append(outputdata)
     return output
-        
