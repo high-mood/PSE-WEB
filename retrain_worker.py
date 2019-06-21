ea30762bce7ffd530442a551b7821bdef85f77f0
Jonasvdham <jonasvdham@gmail.com>
2019-06-21 13:14:49 +0200

    fixed form

--- retrain_worker.py
+++ retrain_worker.py
@@ -1,11 +1,13 @@
 import numpy as np
 from sklearn.ensemble import GradientBoostingRegressor as GBR
-from joblib import dump, load
+from joblib import dump
 from app.utils.tasks import link_features_mood
+
 
 def main():
     """Retrain the ML model on new data in the database, generated through user-feedback"""
-    features = ["response_excitedness", "response_happiness", "mode", "time_signature", "acousticness", "danceability", "energy", "instrumentalness", "liveness", "loudness", "speechiness", "valence", "tempo"]
+    features = ["response_excitedness", "response_happiness", "mode", "time_signature", "acousticness",
+                "danceability", "energy", "instrumentalness", "liveness", "loudness", "speechiness", "valence", "tempo"]
     data = link_features_mood(get_responses=True)
     trainset = []
     print("test")
@@ -24,7 +26,6 @@ def main():
     H_est.fit(traindata, happiness)
     dump(E_est, 'Retrained-Energy.joblib')
     dump(H_est, 'Retrained-Happiness.joblib')        
-
 
 if __name__ == "__main__":
     main()
