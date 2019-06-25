b6b3ff51727a158bf4f010313a4661f24ff5a39e
Diallo <mnl.diallo@gmail.com>
2019-06-25 13:59:16 +0200

    Added comments to hourly API

--- app/API/user_calls.py
+++ app/API/user_calls.py
@@ -155,17 +155,14 @@ def valid_hour(time, start_hour, end_hour):
 @api.response(400, 'Invalid date')
 @api.response(404, 'No moods found')
 class HourlyMood(Resource):
-    """NEW API THAT RETURNS LIST OF AVERAGES PER HOUR
+    """
+    This API returns the hourly mood of a user specified within a timeframe. Thus over the entire history, we take the
+    average for each hour within the specified hourly timeframe.
+    """
 
-BV 12-13 voor GEHELE HISTORY (DUS ALLE METRICS EN DE MOOD)
-
-VOOR DE GEHELE TIMEFRAME DIE IS OPGEGEVEN EN DAN PER UUR
-
-
-"""
+    # Output format
     hourly_mood = api.model('Mood over hour', {
         'userid': fields.String,
-
         'hours': fields.Nested(api.model('metrics_mood', {
             "hour": fields.String,
             "excitedness": fields.Float,
@@ -186,7 +183,6 @@ VOOR DE GEHELE TIMEFRAME DIE IS OPGEGEVEN EN DAN PER UUR
     })
 
     @api.marshal_with(hourly_mood, envelope='resource')
-    # ALL ZERO PADDED TODO DOCUMENT
     def get(self=None, userid="snipy12", start=3, end=24, endoftime=False):
         """
         Obtain moods of a user within a given time frame in hours of a day.
@@ -205,43 +201,65 @@ VOOR DE GEHELE TIMEFRAME DIE IS OPGEGEVEN EN DAN PER UUR
         songs = influx.get_songs(client, userid)
 
         if songs:
+            # Extract all songs that fit within the time-frame specified.
             relevantSongs = [val for val in songs if valid_hour(val['time'], start, end)]
+
+            # Create a dictionary of lists to store the songid's per hour
+            # Thus {"Time":[songids,....]}
             resultDict = defaultdict(list)
+
+            # For each {songid,time} in the list
             for song in relevantSongs:
                 mood_time = song['time'].split(".")[0]
                 mood_time = datetime.datetime.strptime(mood_time[:-1], '%Y-%m-%dT%H:%M:%S')
                 mood_hour = mood_time.hour
 
+                # TODO: This is always true so duplicate code
                 if start <= mood_hour <= end:
+                    # Add the songid to the dictionary
                     resultDict[mood_hour].append(song['songid'])
 
             results = []
+            # With the list of IDs with corresponding hour and features.
             for time, songid_list in resultDict.items():
+                # Obtain the metrics for each song inside a list of songs.
                 songs = models.Song.get_songs_with_mood(songid_list)
 
                 tempresults = []
+
                 for song in songs:
+                    # We want both the mood and metrics thus we convert the object to a dictionary and combine them
+                    # Then we pop the keys of said dictionary.
                     temp1 = ((song[0].__dict__))
                     temp2 = ((song[1].__dict__))
                     combineddict = {**temp1, **temp2}
                     combineddict.pop("name")
                     combineddict.pop("_sa_instance_state")
                     combineddict.pop("songid")
+
+                    # Add the metrics of a SINGLE song to the temporary result
                     tempresults.append(combineddict)
 
+                # As we are interested in the average we store the count of this list
                 count = len(tempresults)
+                # Remove the first element, which will be used for addition
                 A = Counter(tempresults.pop(0))
+                # Now iterate over each song's metrics within the results and add the values using counter.
                 for B in tempresults:
                     B = Counter(B)
                     A = A + B
 
+                # Convert these to averages by dividing each key if possible
                 for key, value in A.items():
                     if value:
                         A[key] = value / count
                     else:
                         A[key] = 0
+
+                # Add hour key for correct output format and append to results
                 A['hour'] = time
                 results.append(A)
+
             return {"userid": userid,
                     "hours": results}
 
