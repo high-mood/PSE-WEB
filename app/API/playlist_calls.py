a30aa06f60eac7bacffe55e540d7612740f4023a
Stan van den Broek <stan.vandenbroek@student.uva.nl>
2019-06-25 15:24:59 +0200

    you should not use this ffs

--- app/API/playlist_calls.py
+++ app/API/playlist_calls.py
@@ -29,6 +29,8 @@ class PlaylistList(Resource):
     @api.marshal_with(playlists, envelope='resource')
     def get(self, userid):
         """Get playlist """
+        # TODO You should just call the spotify api!
+        api.abort(403, message=f"Just call the Spotify API")
         playlists = spotify.get_playlists(userid)
         return {
             'userid': userid,
