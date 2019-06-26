b775dd292c325b04f2be4f076589e384000a9c4a
Youri Reijne <y.reijne@gmail.com>
2019-06-26 15:50:33 +0200

    added name to metric, updated topsongs to include moods

--- app/API/playlist_calls.py
+++ app/API/playlist_calls.py
@@ -1,30 +1,26 @@
-import json
-
-import requests
 from flask_restplus import Namespace, Resource, fields
-
 from app.utils import spotify
-from app.utils.models import User
 
 api = Namespace('playlist', description='playlist', path="/playlist")
-
-playlists = api.model('Playlist details', {
-    'userid': fields.String,
-    'playlists': fields.Nested(api.model('playlist',
-                                         {"name": fields.String,
-                                          "id": fields.String,
-                                          'href': fields.String,
-                                          'public': fields.Boolean,
-                                          'track_href': fields.String,
-                                          'track_count': fields.Integer, }
-
-                                         ))
-})
 
 
 @api.route('/<string:userid>/list/')
 class PlaylistList(Resource):
     """Obtain a list of the user's playlist"""
+
+    # Output format
+    playlists = api.model('Playlist details', {
+        'userid': fields.String,
+        'playlists': fields.Nested(api.model('playlist',
+                                             {"name": fields.String,
+                                              "id": fields.String,
+                                              'href': fields.String,
+                                              'public': fields.Boolean,
+                                              'track_href': fields.String,
+                                              'track_count': fields.Integer, }
+
+                                             ))
+    })
 
     @api.marshal_with(playlists, envelope='resource')
     def get(self, userid):
