e077992aa20c9d5b39a921f4ccf28ac0afc21541
Stan van den Broek <stan.vandenbroek@student.uva.nl>
2019-06-27 15:01:07 +0200

    fixed pep8 errors

--- app/utils/models.py
+++ app/utils/models.py
@@ -271,10 +271,9 @@ class Songmood(db.Model):
             response_excitedness = songmood.response_excitedness
             response_happiness = songmood.response_happiness
             response_count = songmood.response_count
-            songmood.response_happiness = (response_happiness * response_count + user_happiness) / (
-                    response_count + 1)
+            songmood.response_happiness = (response_happiness * response_count + user_happiness) / (response_count + 1)
             songmood.response_excitedness = (response_excitedness * response_count + user_excitedness) / (
-                    response_count + 1)
+                        response_count + 1)
             songmood.response_count = response_count + 1
 
             db.session.commit()
